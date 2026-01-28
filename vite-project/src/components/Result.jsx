96046af8f0933ab0395b5830f1f0cf16f08d3f91
aarondavidreal-lab <aarondavid.real@gmail.com>
2026-01-28 15:16:28 +0100

    style resutl-section and summary-section

--- vite-project/src/components/Result.jsx
+++ vite-project/src/components/Result.jsx
@@ -10,25 +10,30 @@ export default function Result() {
         icon={dat.icon}
         category={dat.category}
         score={dat.score}
+        color={dat.color}
+        backgroundColor={dat.backgroundColor}
         key={dat.category}
     />)
 
     return (
         <div className="container">
-            <div className="result">
+
+            <section className="result-section">
                 <h1>Your Result</h1>
-                <div className="score">
-                    <span>76</span>
-                    <span>of 100</span>
+                <div className="overall-score">
+                    <span className="overall-score-span">76</span>
+                    <span className="percent">of 100</span>
                 </div>
-                <p>Great</p>
-                <p>you scored higher than 65% of the people who have taken these tests.</p>
-            </div>
-            <div className="summary">
-                <h1>Summary</h1>
+                <p className="great">Great</p>
+                <p className="note">You scored higher than 65% of the people who have taken these tests.</p>
+            </section>
+
+            <section className="summary-section">
+                <h2>Summary</h2>
                 {categories}
                 <button>Continue</button>  
-            </div>
+            </section>
+            
         </div>
     )
 }
--- vite-project/src/components/Stats.jsx
+++ vite-project/src/components/Stats.jsx
@@ -1,14 +1,14 @@
 
 
-export default function Stats(props) {
-
-    
-
-    return(
-            <div className="stat">
-                <img src={props.icon} alt={props.category}/>
-                <p>{props.category}</p>
-                <p>{props.score}<span>/100</span></p>
-            </div>
+export default function Stats(props) {    
+   return(
+    <div className="stat" style={{backgroundColor: props.backgroundColor}}>
+        <div>
+            <img src={props.icon} alt={props.category}/>
+            <p style={{color: props.color, }}>{props.category}</p>
+        </div>
+        
+        <p className="score">{props.score} <span>/ 100</span></p>
+    </div>
     )
 }
